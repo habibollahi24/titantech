3e8ceab70f834b7e2d9b9a8a6510174465d2f493
mohammad <habibollahi24@gmail.com>
2023-11-13 15:56:51 +0330

    remove files

--- routes/post/index.js
+++ routes/post/index.js
@@ -3,12 +3,6 @@ const {
   createPost,
   getPosts,
   getPost,
-  getUserPost,
-  getComments,
-  likePost,
-  likePostComment,
-  commentPost,
-  replyPostComment,
   deletePost,
 } = require("../../controllers/post");
 const authMiddleware = require("../../middleware/authMiddleware");
@@ -18,18 +12,9 @@ const router = express.Router();
 router.post("/create-post", authMiddleware, createPost);
 // get posts
 router.get("/", authMiddleware, getPosts);
+
+//get post
 router.get("/:id", authMiddleware, getPost);
-
-router.post("/get-user-post/:id", authMiddleware, getUserPost);
-
-// get comments
-router.get("/comments/:postId", authMiddleware, getComments);
-
-//like and comment on posts
-router.post("/like/:id", authMiddleware, likePost);
-router.post("/like-comment/:id/:rid?", authMiddleware, likePostComment);
-router.post("/comment/:id", authMiddleware, commentPost); //comment on post
-router.post("/reply-comment/:id", authMiddleware, replyPostComment);
 
 //delete post
 router.delete("/:id", authMiddleware, deletePost);
--- routes/user/index.js
+++ routes/user/index.js
@@ -1,29 +1,9 @@
 const express = require("express");
-const {
-  getUser,
-  updateUser,
-  friendRequest,
-  getFriendRequest,
-  acceptRequest,
-  profileViews,
-  suggestedFriends,
-} = require("../../controllers/user");
+const { getUser, updateUser } = require("../../controllers/user");
 const authMiddleware = require("../../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/user/:id?", authMiddleware, getUser);
 router.put("/update", authMiddleware, updateUser);
-// friend request
-router.post("/friend-request", authMiddleware, friendRequest);
-router.post("/get-friend-request", authMiddleware, getFriendRequest);
-
-// accept / deny friend request
-router.post("/accept-request", authMiddleware, acceptRequest);
-
-// view profile
-router.post("/profile-view", authMiddleware, profileViews);
-
-//suggested friends
-router.post("/suggested-friends", authMiddleware, suggestedFriends);
 
 module.exports = router;
